6d2981888a75479644c21efe1eee8e99e3e7eee9
Arlie McCarthy <arliemccarthy@gmail.com>
2021-06-10 17:23:52 +0100

    Analysis re-run after updates to input data sheet and species names

--- scripts/2-JoshAnalysis.R
+++ scripts/2-JoshAnalysis.R
@@ -10,7 +10,7 @@ community_matrix_sites <- community_matrix %>%
   rownames_to_column(var = "id") %>% 
   left_join(site_info)
 #Re-extract community matrix and site info data now they are aligned
-community_matrix_AB <- select(community_matrix_sites, c_f_corophium_na:conchoderma_auritum)
+community_matrix_AB <- select(community_matrix_sites, -id)
 community_site_info <- select(community_matrix_sites, id, area, stage, quadrat_number)
 
 #Make a presence-absence version of community data
@@ -26,20 +26,26 @@ community_matrix_PA <- decostand(community_matrix_AB, method = "pa")
 ABpermanova <- adonis2(community_matrix_AB ~ stage, community_site_info, permutations=1000,
                        method="bray")
 ABpermanova
-##         Df SumOfSqs      R2      F Pr(>F)
-##stage     1   0.4827 0.03656 1.4798 0.1249
-##Residual 39  12.7221 0.96344              
-##Total    40  13.2048 1.00000
-#Cannot reject null hypothesis that centroids and/or dispersion of the groups are the same 
+#         Df SumOfSqs      R2      F  Pr(>F)  
+#stage     1   0.8247 0.08526 3.6351 0.01099 *
+#Residual 39   8.8477 0.91474                 
+#Total    40   9.6724 1.00000                 
+#---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#CAN reject null hypothesis? 
+
+#So, unlike previous time the analysis was run (on data without verified species), 
+#we can detect overall differences between stages on the abundance matrix.
+
 
 #PERMANOVA for presence-absence data
 PApermanova <- adonis2(community_matrix_PA ~ stage, community_site_info, permutations=1000,
                        method="bray")
 PApermanova
 ##         Df SumOfSqs      R2      F   Pr(>F)    
-##stage     1   1.3602 0.15336 7.0643 0.000999 ***
-##Residual 39   7.5093 0.84664                    
-##Total    40   8.8695 1.00000 
+##stage     1   0.4618 0.15706 7.2668 0.000999 ***
+##Residual 39   2.4784 0.84294                    
+##Total    40   2.9402 1.00000  
 #CAN reject null hypothesis.
 
 #So basically: heaps of variation in abundances WITHIN each stage (cannot detect overall
@@ -58,53 +64,56 @@ PApermanova
 #Use least-constrained null model (only total count maintained)
 
 oecosimu(community_matrix_PA, nestednodf, "r00", nsimul=1000)
-##N columns  : 42.16523 
-##N rows     : 60.64611 
-##NODF       : 51.87331 
-##Matrix fill: 0.1651032 
-##
-##          statistic    SES   mean   2.5%    50%  97.5% Pr(sim.)    
-##N.columns    42.165 28.000 17.811 16.148 17.794 19.608 0.000999 ***
-##N.rows       60.646 48.669 17.836 16.199 17.790 19.582 0.000999 ***
-##NODF         51.873 41.577 17.824 16.283 17.798 19.442 0.000999 ***
+#N columns  : 51.34796 
+#N rows     : 80.44558 
+#NODF       : 65.89677 
+#Matrix fill: 0.2355741 
+#
+#statistic    SES   mean   2.5%    50%  97.5% Pr(sim.)    
+#N.columns    51.348 28.868 24.819 23.065 24.816 26.638 0.000999 ***
+#N.rows       80.446 60.828 24.828 23.000 24.850 26.632 0.000999 ***
+#NODF         65.897 47.811 24.823 23.191 24.812 26.545 0.000999 ***
 
 #Use null model where individual site richness is constrained
 
 oecosimu(community_matrix_PA, nestednodf, "r0", nsimul=1000)
-##N columns  : 42.16523 
-##N rows     : 60.64611 
-##NODF       : 51.87331 
-##Matrix fill: 0.1651032 
-##
-##          statistic    SES   mean   2.5%    50%  97.5% Pr(sim.)    
-##N.columns    42.165 17.995 23.324 21.299 23.321 25.460 0.000999 ***
-##N.rows       60.646 45.033 21.584 20.011 21.537 23.582 0.000999 ***
-##NODF         51.873 34.233 22.410 20.750 22.379 24.143 0.000999 ***
+#N columns  : 51.34796 
+#N rows     : 80.44558 
+#NODF       : 65.89677 
+#Matrix fill: 0.2355741 
+#
+#statistic    SES   mean   2.5%    50%  97.5% Pr(sim.)    
+#N.columns    51.348 26.839 27.561 25.865 27.559 29.280 0.000999 ***
+#N.rows       80.446 95.989 27.882 26.930 27.839 29.011 0.000999 ***
+#NODF         65.897 55.755 27.721 26.489 27.713 29.066 0.000999 ***
 
 #Use null model where species prevalences are constrained
 
 oecosimu(community_matrix_PA, nestednodf, "c0", nsimul=1000)
-##N columns  : 42.16523 
-##N rows     : 60.64611 
-##NODF       : 51.87331 
-##Matrix fill: 0.1651032 
-##
-##          statistic     SES   mean   2.5%    50%  97.5% Pr(sim.)    
-##N.columns    42.165 15.4292 26.032 24.124 26.010 28.125 0.000999 ***
-##N.rows       60.646  8.2024 43.598 39.589 43.577 47.738 0.000999 ***
-##NODF         51.873 11.3218 35.259 32.480 35.240 38.271 0.000999 ***
+#N columns  : 51.34796 
+#N rows     : 80.44558 
+#NODF       : 65.89677 
+#Matrix fill: 0.2355741 
+#
+#statistic     SES   mean   2.5%    50%  97.5% Pr(sim.)    
+#N.columns    51.348 14.9415 37.895 36.257 37.858 39.842 0.000999 ***
+#N.rows       80.446  7.6746 62.071 57.127 62.233 66.400 0.000999 ***
+#NODF         65.897 10.4494 49.983 47.015 50.038 52.768 0.000999 ***
+
 
 #Use null model where both site richness and species prevalences are constrained 
 oecosimu(community_matrix_PA, nestednodf, "quasiswap", nsimul=1000)
-##N columns  : 42.16523 
-##N rows     : 60.64611 
-##NODF       : 51.87331 
-##Matrix fill: 0.1651032 
-##
-##          statistic      SES   mean   2.5%    50%  97.5% Pr(sim.)  
-##N.columns    42.165  1.64850 40.140 37.600 40.238 42.292  0.07093 .
-##N.rows       60.646 -1.48999 63.236 59.253 63.420 66.074  0.15485  
-##NODF         51.873 -0.33869 52.273 49.825 52.353 54.333  0.68831
+#N columns  : 51.34796 
+#N rows     : 80.44558 
+#NODF       : 65.89677 
+#Matrix fill: 0.2355741 
+
+#statistic      SES   mean   2.5%    50%  97.5% Pr(sim.)  
+#N.columns    51.348  1.34289 50.143 48.245 50.204 51.757  0.16484  
+#N.rows       80.446 -1.86316 80.961 80.342 80.990 81.404  0.08891 .
+#NODF         65.897  0.69391 65.552 64.467 65.581 66.402  0.51049  
+#---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
 
 #So basically: we observe that species-poor sites tend to be nested subsets of species-rich
